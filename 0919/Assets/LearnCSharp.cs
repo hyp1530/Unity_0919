e3c71ecc1a86dc9861faa31360cbc26064925996
hyp1530 <55218857+hyp1530@users.noreply.github.com>
2019-10-03 10:41:38 +0800

    1003

---
+++ 0919/Assets/LearnCSharp.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LearnCSharp : MonoBehaviour {
+    [Header("分數")]
+    public int score = 80;
+    [Header("速度")]
+    public float speed = 10f;
+    [Header("道具")]
+    public string prop = "藥水";
+    [Header("任務")]
+    public bool mission = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
