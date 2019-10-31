2956a79a47c73d1b0416800f5c00b076620f37d3
hyp1530 <55218857+hyp1530@users.noreply.github.com>
2019-10-31 10:24:24 +0800

    1031角色腳本控制

---
+++ 0919/Assets/Lady.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lady : MonoBehaviour {
+    private void Run()
+    {
+
+    }
+    private void Jump()
+    {
+
+    }
+    private void Atk()
+    {
+
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
